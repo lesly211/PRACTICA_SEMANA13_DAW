2d12be7fe1f6ac1d986a7e87a37ca0517cd60bc9
Lesly Brenda Navarro Serva <leslynavarro804@gmail.com>
2026-07-02 10:51:37 -0500

    CAMBIOS

--- lotes/urls.py
+++ lotes/urls.py
@@ -1,9 +1,6 @@
 from rest_framework.routers import DefaultRouter
 from .views import LoteForestalViewSet
 
-# FASE 3 - DefaultRouter genera automaticamente todas las rutas del
-# ModelViewSet (list, detail, y los @action definidos) y mantiene el
-# principio DRY: no se escribe una URLConf manual por cada endpoint.
 router = DefaultRouter()
 router.register(r'lotes', LoteForestalViewSet, basename='lote')
 

2808d8ccf7f3dbf43d758bd4f37d3b153a6932ca
Lesly Brenda Navarro Serva <leslynavarro804@gmail.com>
2026-07-02 11:04:09 -0500

    CAMBIO5

--- trazabilidad_project/urls.py
+++ trazabilidad_project/urls.py
@@ -7,11 +7,9 @@ from drf_spectacular.views import (
 urlpatterns = [
     path('admin/', admin.site.urls),
 
-    # FASE 3 - API (incluye rutas generadas por el DefaultRouter)
     path('api/', include('lotes.urls')),
     path('api-auth/', include('rest_framework.urls')),  # login/logout navegable
 
-    # FASE 5 - Documentacion OpenAPI/Swagger automatica (drf-spectacular)
     path('api/schema/', SpectacularAPIView.as_view(), name='schema'),
     path('api/docs/', SpectacularSwaggerView.as_view(url_name='schema'), name='swagger-ui'),
     path('api/redoc/', SpectacularRedocView.as_view(url_name='schema'), name='redoc'),
